f02dbe67238483c7d96330f248456c295fa0f766
air2999 <user_202248@outlook.com>
2025-06-21 22:15:53 +0800

    refactor

--- api/src/auth/storage.ts
+++ api/src/auth/storage.ts
@@ -1,21 +1,24 @@
 import path from 'path';
 import fs from 'fs'
 
-export const dbPath = path.resolve(__dirname, '../../database/usersLoginInformation.json');
+export const dbDirPath = path.resolve(__dirname, '../../database');
+export const dbPath = path.resolve(dbDirPath, './usersLoginInformation.json');
 
 export class UserLoginInformation {
     constructor(public pwd: string, public lastLoginTime: number) {}
 }
 
 let userDB;
-fs.readFile(dbPath, 'utf-8', (err, data) => {
-    if (err) {
-        fs.writeFileSync(dbPath, '[]')
-        userDB = []
-        return;
-    }
-    userDB = JSON.parse(data);
-})
+
+if (!fs.existsSync(dbDirPath)) {
+    fs.mkdirSync(dbDirPath, { recursive: true })
+}
+if (fs.existsSync(dbPath)) {
+    userDB = JSON.parse(fs.readFileSync(dbPath, 'utf-8', ))
+} else {
+    fs.writeFileSync(dbPath, '[]')
+    userDB = []
+}
 
 export const userMap = new Map<string, UserLoginInformation>(userDB /* entries */);
 
